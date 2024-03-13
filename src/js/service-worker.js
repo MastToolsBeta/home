be10eab3e6088b528d976c628c0c8024750e559a
MastTools Beta <masttools.com+1@gmail.com>
2024-03-13 20:33:07 +0530

    ghfhjgh

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -1,24 +1,21 @@
-// Add this part to register the beforeinstallprompt event
+
 let deferredPrompt;
 
 self.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent Chrome 76 and later from automatically showing the prompt
+ 
   event.preventDefault();
   
-  // Stash the event so it can be triggered later
+  
   deferredPrompt = event;
   
-  // Optionally, notify the user that your app is installable
-  // You can display a custom UI element to prompt the user to install the app
 });
 
-// The rest of your service worker code...
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('PWA-cache').then((cache) => {
+    caches.open('MastTools.com-cache').then((cache) => {
       return cache.addAll([
         '../../index.html',
-        '../src/img/logo512.png'
+        '../img/logo192.png'
       ]);
     })
   );
